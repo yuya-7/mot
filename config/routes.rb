7d4e10de036d6301e1fab18b355e88af611541c3
yuya-7 <yy0805.alwslv@icloud.com>
2020-06-09 03:41:48 +0900

    user-show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :posts
+  resources :users, only: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_014732) do
+ActiveRecord::Schema.define(version: 2020_06_08_173921) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "place"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_06_08_014732) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
